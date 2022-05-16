5d881c16bf157b33cc6ee35cb6cd8297ec89da8f
Shalllashaska <nickita.burov@gmail.com>
2022-05-16 16:00:19 +0300

    задание react state effect

--- src/Effects.tsx
+++ src/Effects.tsx
@@ -1,5 +1,24 @@
 import { subscribe, unsubscribe } from './resources/API';
+import { useEffect, useState } from 'react';
 
 export function Effects(props: { sourceId: string }) {
-    return <div>123</div>;
+    const [curMessage, newMessage] = useState(-1);
+
+    const callback = (messageId: number) => {
+        newMessage(messageId);
+    };
+
+    useEffect(() => {
+        subscribe(props.sourceId, callback);
+        return () => {
+            newMessage(-1);
+            unsubscribe(props.sourceId, callback);
+        };
+    }, [props.sourceId]);
+
+    return (
+        <div>
+            {props.sourceId}: {curMessage}
+        </div>
+    );
 }
